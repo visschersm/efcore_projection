8f043a193b75947326274bf980c3e5e7609f7c15
Mark Visschers <visschers.m@gmail.com>
2020-10-02 23:49:45 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace efcore_projection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
