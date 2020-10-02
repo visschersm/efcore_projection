5fd241bd5b6f2de2f7938ffe441e08601c924339
Mark Visschers <visschers.m@gmail.com>
2020-10-03 01:12:40 +0200

    Trying to create demo/test project with Sqlite

---
+++ Migrations/20201002230944_initial.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace efcore_projection.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Username = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    Password = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Blogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(nullable: false),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    CreatedById = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blogs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Blogs_Users_CreatedById",
+                        column: x => x.CreatedById,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    CreatedById = table.Column<int>(nullable: false),
+                    BlogId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Posts_Blogs_BlogId",
+                        column: x => x.BlogId,
+                        principalTable: "Blogs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Posts_Users_CreatedById",
+                        column: x => x.CreatedById,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Blogs_CreatedById",
+                table: "Blogs",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_BlogId",
+                table: "Posts",
+                column: "BlogId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_CreatedById",
+                table: "Posts",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+
+            migrationBuilder.DropTable(
+                name: "Blogs");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,212 @@
-﻿using System;
+﻿using System.Runtime.CompilerServices;
+using System.Reflection.Emit;
+using System.Collections.Immutable;
+using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using System.Collections.Generic;
+using Microsoft.Data.Sqlite;
 
-namespace efcore_projection
+var connectionStringBuilder = new SqliteConnectionStringBuilder { DataSource = ":memory:" };
+var connectionString = "Data Source=blogging.db";
+//var connectionString = connectionStringBuilder.ToString();
+var connection = new SqliteConnection(connectionString);
+
+//connection.CreateFunction("getdate", () => DateTime.Now);
+
+var options = new DbContextOptionsBuilder<MyContext>()
+    .UseSqlite(connection)
+    .Options;
+
+using var context = new MyContext(options);
+
+context.Users.AddRange(new User[] 
 {
-    class Program
+    new User
     {
-        static void Main(string[] args)
+        Username = "HarryP",
+        Email = "harry.potter@hogwarts.uk",
+        Password = "expelliarmus",
+        Blogs = new List<Blog> 
         {
-            Console.WriteLine("Hello World!");
+            new Blog
+            {
+                Title = "My biggest accomplishments",
+                Posts = new List<Post> 
+                {
+                    new Post(),
+                    new Post()
+                }
+            }
+        }
+    },
+    new User
+    {
+        Username = "RonaldW",
+        Email = "ronald.weasley@hogwarts.uk",
+        Password = "hermonie",
+        Blogs = new List<Blog>
+        {
+            new Blog
+            {
+                Title = "Why Quidditch is awesome",
+                Posts = new List<Post> 
+                {
+                    new Post(),
+                    new Post()
+                }
+            },
+            new Blog
+            {
+                Title = "Being friends with Harry Potter"
+            }
+        }
+    },
+    new User
+    {
+        Username = "HermonieG",
+        Email = "hermonie.granger@hogwarts.uk",
+        Password = "idJcZF68uqx4sn3F47S7a7dDJqDM7F",
+        Blogs = new List<Blog>
+        {
+            new Blog
+            {
+                Title = "Proof of alternate universes",
+                Posts = new List<Post> 
+                {
+                    new Post(), 
+                    new Post(),
+                    new Post(),
+                }
+            },
+            new Blog
+            {
+                Title = "Missed muggle influences in the magical world",
+                Posts = new List<Post>
+                {
+                    new Post(),
+                    new Post(),
+                    new Post(),
+                    new Post()
+                }
+            }
         }
     }
+});
+
+context.SaveChanges();
+
+// TODO:    Find a nice way to project entities 
+//          with foreign relations to views.
+var blog = context.Blogs.AsNoTracking()
+    .Where(blog => blog.Id == 1)
+    //.Select(BlogMapper.Projection)
+    .SingleOrDefault();
+
+
+
+public class MyContext : DbContext
+{
+    public DbSet<Blog> Blogs => Set<Blog>();
+    public DbSet<Post> Posts => Set<Post>();
+    public DbSet<User> Users => Set<User>();
+
+    public MyContext()
+        : base() 
+    {
+
+    }
+
+    public MyContext(DbContextOptions options)
+        : base(options)
+    {
+        
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder builder)
+    {
+        if(!builder.IsConfigured)
+        {
+            builder.UseSqlite("Data Source=blogging.db");
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.Property(user => user.Username)
+                .IsRequired();
+
+            entity.HasIndex(user => user.Email)
+                .IsUnique();
+
+            entity.Property(user => user.Password)
+                .IsRequired();
+        });
+        
+        modelBuilder.Entity<Blog>(entity =>
+        {
+            entity.Property(blog => blog.Title)
+                .IsRequired();
+            
+            // entity.Property(blog => blog.CreatedDate)
+            //     .HasDefaultValueSql("getdate()");
+            
+            entity.HasOne(blog => blog.CreatedBy)
+                .WithMany(user => user.Blogs)
+                .HasForeignKey(blog => blog.CreatedById);
+        });
+
+        modelBuilder.Entity<Post>(entity =>
+        {
+            //entity.Property(post => post.CreatedDate)
+            //    .HasDefaultValueSql("getdate()");
+
+            entity.HasOne(post => post.Blog)
+                .WithMany(blog => blog.Posts)
+                .HasForeignKey(post => post.BlogId);
+        });
+    }
 }
+
+public class Blog
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+
+    public DateTime CreatedDate { get; set; }
+    public int CreatedById { get; set; }
+    public User CreatedBy { get; set; } = null!;
+
+    public List<Post> Posts { get; set; } = new List<Post>();
+}
+
+public class Post
+{
+    public int Id { get; set; }
+
+    public DateTime CreatedDate { get; set; }
+    public int CreatedById { get; set; }
+    public User CreatedBy { get; set; } = null!;
+
+    public int BlogId { get; set; }
+    public Blog Blog { get; set; } = null!;
+    
+}
+
+public class User
+{
+    public int Id { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+
+    public List<Blog> Blogs { get; set; } = new List<Blog>();
+}
+
+// public static class BlogMapper
+// {
+//     public Expression<Func<Blog, BlogView>> Projection { get; set; }
+// }
